2e61af21ea835c0da259db61ce00e41574e9b4f6
yfan2012 <yfan2012@gmail.com>
2021-05-13 16:33:54 -0400

    add abundance ratios using total number of reads

--- length_ratios.R
+++ length_ratios.R
@@ -146,3 +146,99 @@ ggplot(ratios, aes(x=dpi, y=subgen, colour=condition, fill=condition, alpha=.2))
 dev.off()
 
 
+
+
+
+
+####ratios based on total reads as suggested by jane
+ratios=tibble(name=as.character(),
+              full=as.numeric(),
+              subgen=as.numeric(),
+              dvg=as.numeric(),
+              subdvg=as.numeric(),
+              condition=as.character(),
+              dpi=as.numeric())
+
+for (i in 1:(dim(sampinfo)[1]-1)) {
+    info=sampinfo[i,]
+    samp=paste0(info$condition, 'dpi', as.character(info$dpi), '_rep', as.character(info$rep))
+    paffile=file.path(datadir, samp, 'align', paste0(samp, '.paf'))
+
+    lenspaf=read_tsv(paffile, col_names=pafcols) %>%
+        filter(mapq>20) %>%
+        select(qname, qlen, rname, rlen, rstart, rend, mapq, type)
+    totlen=dim(lenspaf)[1]
+    
+    full=lenspaf %>%
+        filter(rstart<7000) %>%
+        filter(rend>3000)
+    totfull=dim(full)[1]
+    #defining drops are 7608 to 11694
+    subgen=lenspaf %>%
+        filter(rstart>7610 & rstart<11690)
+    totsubgen=dim(subgen)[1]
+    ##dvg drop at 1751
+    dvg=lenspaf %>%
+        filter(rend>1749 & rend<1755)
+    totdvg=dim(dvg)[1]
+    subdvg=lenspaf %>%
+        ##filter(qlen>100) %>%
+        ##filter(rstart<50) %>%
+        filter(rend>50 & rend<1745)
+    totsubdvg=dim(subdvg)[1]
+
+    full_ratio=totfull/totlen
+    subgen_ratio=totsubgen/totlen
+    dvg_ratio=totdvg/totlen
+    subdvg_ratio=totsubdvg/totlen
+
+    ratioinfo=tibble(name=samp,
+                     full=full_ratio,
+                     subgen=subgen_ratio,
+                     dvg=dvg_ratio,
+                     subdvg=subdvg_ratio,
+                     condition=info$condition,
+                     dpi=info$dpi)
+    ratios=bind_rows(ratios, ratioinfo)
+}
+
+ratios$dpi=as.character(ratios$dpi)
+speciesyieldpdf=file.path(dbxdir, 'rna_species_ratios_numreads.pdf')
+pdf(speciesyieldpdf, h=5, w=17)
+fullplot=ggplot(ratios, aes(x=dpi, y=full, colour=condition, fill=condition, alpha=.2)) +
+    geom_jitter(size=6, width=.05) +
+    ggtitle('Genomic RNA') +
+    scale_fill_brewer(palette='Set2') +
+    scale_colour_brewer(palette='Set2') +
+    theme_bw() +
+    theme(panel.grid.minor.x=element_blank(), panel.grid.minor.y=element_blank())
+dvgplot=ggplot(ratios, aes(x=dpi, y=dvg, colour=condition, fill=condition, alpha=.2)) +
+    geom_jitter(size=6, width=.05) +
+    ggtitle('DVG') +
+    scale_fill_brewer(palette='Set2') +
+    scale_colour_brewer(palette='Set2') +
+    theme_bw() +
+    theme(panel.grid.minor.x=element_blank(), panel.grid.minor.y=element_blank())
+subdvgplot=ggplot(ratios, aes(x=dpi, y=subdvg, colour=condition, fill=condition, alpha=.2)) +
+    geom_jitter(size=6, width=.05) +
+    ggtitle('Sub DVG') +
+    scale_fill_brewer(palette='Set2') +
+    scale_colour_brewer(palette='Set2') +
+    theme_bw() +
+    theme(panel.grid.minor.x=element_blank(), panel.grid.minor.y=element_blank())
+subgenplot=ggplot(ratios, aes(x=dpi, y=subgen, colour=condition, fill=condition, alpha=.2)) +
+    geom_jitter(size=6, width=.05) +
+    ggtitle('Subgenomic') +
+    scale_fill_brewer(palette='Set2') +
+    scale_colour_brewer(palette='Set2') +
+    theme_bw() +
+    theme(panel.grid.minor.x=element_blank(), panel.grid.minor.y=element_blank())
+plot_grid(fullplot, dvgplot, subdvgplot, subgenplot, ncol=4)
+dev.off()
+
+
+
+
+
+
+
